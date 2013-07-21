aeb986c389228b8936bde55e37245e10e2004b62
zhangchen <zhanghen2397@126.com>
2013-07-21 16:28:32 +0800

    update by zhangchen

--- goto_top/README.md
+++ goto_top/README.md
@@ -1,1 +1,72 @@
-#使用方法
+# 图片延时加载组件
+
+## 组件说明
+
+先来看看demo
+
+1. [demo1](http://wenku.baidu.com/pay/index)
+2. [demo2](http://wenku.baidu.com/vip/index)
+3. [demo3](http://wenku.baidu.com/vip/privilege)
+
+图片延时加载组件主要是为了提高页面的性能，加快页面可视区域中图片的加载速度，当页面中存在大量图片时，采用此组件非常适合。其基本原理是，将图片的真实路径不直接写在`src`上，先赋值给一个自定义属性上，如`data-src`，当用户滚动到该图片的可视区域时，通过`data-src`取出图片的真实路径，替换`src`的1X1px的透明小图。
+
+html结构：
+```html
+<img data-src="./images/1.jpg" src="./images/blank.gif" />
+```
+
+## 实现过程
+
+主要配置项：
+```javascript
+this.defaultConfig = {
+    lazyLoadAttr: 'data-src',      //需要延时加载的图片的自定义属性
+    preloadHeight: 0,              //预加载高度
+    loadedCallback: null           //加载完成后的回调
+};
+
+//自定义事件
+var lazyLoadIns = new lazyLoad();
+
+//图片加载完成后的事件监听
+lazyLoadIns.on( 'afterLoaded', function( data ) {
+    var curImg = data.item
+    //code here
+} );
+```
+
+默认情况下，当图片处于可视区域时，采用的是`jQuery`的.fadeIn( 'slow' )来显示图片，如果配置了loadedCallback，则采用用户自定义配置的方式显示图片。
+
+根据以上的实现原理，主要需要计算可视区域内图片，当图片的上边缘的坐标小于scrollTop + viewHeight且图片的下边缘的坐标大于scrollTop时，刚显示该图片（这里只考虑了垂直方向上的计算，如果有水平方向上的滚动，也需要将水平方向上的坐标计算考虑进去）。实现代码如下：
+
+```javascript
+$.each( this.lazyImgs, function( index, item ) {
+    var itemPosY = item.offset().top,
+        itemPosDepY = itemPosY + item.innerHeight(),
+        imgSrc = item.attr( lazyLoadAttr );
+
+    if ( itemPosY < viewOffset && itemPosDepY > scrollOffset && imgSrc ) {
+        item.css( 'display', 'none' );
+
+        item.attr( 'src', imgSrc );
+        item.removeAttr( lazyLoadAttr );
+        me.lazyImgsLen--;
+
+        $( me ).trigger( {
+            type: 'afterLoaded',
+            img: item
+        } );
+    }
+} );
+```
+
+## 使用
+
+```javascript
+new lazyLoad( {
+    lazyLoadAttr: 'data-src',
+    preloadHeight: 200
+} );
+```
+
+[查看demo](http://zhangchen2397.github.io/component/lazy_load/demo/)
---
+++ goto_top/goto_top.js
@@ -0,0 +1,142 @@
+/**
+ * gotoTop ui component
+ * by: zhangchen
+ * weibo: @zhangchen2397
+ * blog: blog.huitaoba.com
+ * mail: zhangchen2397@gmail.com
+ */
+
+( function( root ) {
+    var $ = root.$;
+
+    if ( typeof require !== 'undefined' ) {
+        $ = require( 'jquery' );
+    }
+
+    //返回顶部元素模板
+    var tpl = [
+        '<div class="goto-top" id="goto-top">',
+            '<a href="javascript:void(0);">返回顶部</a>',
+        '</div>'
+    ].join( '' );
+
+    var gotoTop = function( config ) {
+        this.defaultConfig = {
+            el: 'goto-top',
+            containerWidth: 0,          //容器宽度(如果为0，则相对于body定位)
+            showBtnScrollHeight: 100,   //显示按钮的最小scollTop值
+            minViewWidth: 1024,         //最小可视宽度
+            excursion: { 
+                "bottom": 100,          //相对于body的底偏移量
+                "left": 12,             //相对于容器的左偏移量
+                "right": 20             //相对于body的右偏移量
+            }
+        };
+
+        this.config = $.extend( true, this.defaultConfig, config || {} );
+
+        this.init.call( this );
+    };
+
+    $.extend( gotoTop.prototype, {
+        init: function() {
+            this._createBtnEl();
+            this._setBtnLeft();
+            this._initEvent();
+        },
+
+        _createBtnEl: function() {
+            var me = this,
+                config = this.config;
+
+            $( document.body ).append( $( tpl ) );
+
+            this.button = $( '#' + config.el );
+        },
+        
+        _initEvent: function(){
+            var me = this,
+                config = this.config;
+
+            $( window ).on( 'resize', function() {
+                me._setBtnLeft();
+                me._setBtnTop();
+            } );
+
+            $( window ).on( 'scroll', function() {
+                me._setShowOrNot();
+                me._setBtnTop();
+            } );
+
+            this.button.on( 'click', function() {
+                window.scrollTo( 0, 0 );
+
+                $( me ).trigger( 'afterScrollTop' );
+            } );
+        },
+
+        _setShowOrNot: function() {
+            var me = this,
+                config = this.config,
+                button = this.button,
+                scrollTop = $( window ).scrollTop();
+
+            if ( scrollTop > 100 ) {
+                button.css( 'display', 'block' );
+            } else {
+                button.css( 'display', 'none' );
+            }
+        },
+
+        _setBtnLeft: function() {
+            var me = this,
+                config = this.config,
+                containerWidth = config.containerWidth,
+                excursion = config.excursion,
+                viewportWidth = $( window ).width(),
+                leftP = 0;
+
+            if ( containerWidth ) {
+                leftP = ( viewportWidth - containerWidth ) / 2 + containerWidth + excursion.left;
+
+                //当显示区域的宽度小于配置的最小宽度时，left坐标有变化
+                if ( viewportWidth < config.minViewWidth ) {
+                    leftP = viewportWidth - 70;
+                }
+
+                this.button.css( 'left', leftP );
+            } else {
+                this.button.css( 'right', excursion.right );
+            }
+        },
+
+        _setBtnTop: function() {
+            var me = this,
+                config = this.config,
+                button = this.button,
+                excursion = config.excursion,
+                viewportHeight = $( window ).height();
+
+            //如果是ie6
+            if ( typeof document.body.style.maxHeight === 'undefined' )  {
+                button.css( {
+                    'position': 'absolute',
+                    'top': viewportHeight + $( window ).scrollTop() - excursion.bottom
+                } );
+            } else {
+                button.css( {
+                    'position': 'fixed',
+                    'bottom': excursion.bottom
+                } );
+            }
+        }
+    } );
+
+    if ( typeof exports !== 'undefined' ) {
+        exports = gotoTop;
+    } else {
+        root.gotoTop = gotoTop;
+    }
+
+} )( this );
+
